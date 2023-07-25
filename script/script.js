5536916ac1d499a8948d194e0abab1e91c79fd54
Seth Mack <sethvmack@gmail.com>
2023-07-25 16:18:59 +0100

    Added footer and main body to index

---
+++ script/script.js
@@ -0,0 +1,20 @@
+var ready = (callback) => {
+    if (document.readyState != "loading") callback();
+    else document.addEventListener("DOMContentLoaded", callback);
+}
+ready(() => {
+    document.querySelector(".header").style.height = window.innerHeight + "px";
+})
+
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
